457a7e9a91e6b79763c42a30e7d1532ba476b21e
7raul7k <balanescuraul@gmail.com>
2023-08-30 00:55:38 +0300

    add main

---
+++ src/main/java/ro/myclass/onlinebookapi/OnlineBookApiApplication.java
@@ -0,0 +1,13 @@
+package ro.myclass.onlinebookapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/myclass/onlinebookapi/OnlineBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.myclass.onlinebookapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
