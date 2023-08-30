a88a43e0d64c978c199c068e8f8e805bf2a1b1ba
7raul7k <balanescuraul@gmail.com>
2023-08-30 12:02:46 +0300

    add book-resource-test

--- src/main/java/ro/myclass/onlinebookapi/exceptions/ListEmptyException.java
+++ src/main/java/ro/myclass/onlinebookapi/exceptions/ListEmptyException.java
@@ -1,5 +1,10 @@
 package ro.myclass.onlinebookapi.exceptions;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ListEmptyException extends RuntimeException{
 
     public ListEmptyException() {
